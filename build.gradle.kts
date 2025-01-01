26a2515ea47f23509ea5d96bc49562fa2fa16efa
FilatEvgen <sergey.badyanov1@mail.ru>
2025-01-01 19:14:46 +0500

    add unitTest,Actions File

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.ktor)
     alias(libs.plugins.kotlinx.serialization)
     alias(libs.plugins.kotlin.jvm)
+    id("com.gradleup.shadow") version "9.0.0-beta4"
+    id ("java")
 }
 
 group = "org.example"
@@ -14,4 +16,9 @@ repositories {
 tasks.jar {
     archiveBaseName.set("macros_system")
     archiveVersion.set(version.toString())
+}
+tasks.withType<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar> {
+    manifest {
+        attributes["Main-Class"] = "server.ApplicationKt"
+    }
 }
