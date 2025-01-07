bd6f3bbb47ee84fc609a77c7685341fdfff83f1b
FilatEvgen <sergey.badyanov1@mail.ru>
2025-01-07 21:15:29 +0500

    add Actions File

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -20,10 +20,10 @@ dependencies {
 
     implementation(project(":database"))
     // Test implementation
-    testImplementation("io.ktor:ktor-server-test-host:3.0.2")
-    testImplementation("org.jetbrains.kotlin:kotlin-test:2.0.21")
-    testImplementation("io.ktor:ktor-client-mock:3.0.2")
-    testImplementation("com.h2database:h2:2.3.232")
+//    testImplementation("io.ktor:ktor-server-test-host:3.0.2")
+//    testImplementation("org.jetbrains.kotlin:kotlin-test:2.0.21")
+//    testImplementation("io.ktor:ktor-client-mock:3.0.2")
+//    testImplementation("com.h2database:h2:2.3.232")
 }
 
 
