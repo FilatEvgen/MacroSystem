397469264f5e05f893aad0053b5ff7fde6fbfbb9
FilatEvgen <sergey.badyanov1@mail.ru>
2025-01-23 19:11:21 +0500

    first commit

--- authorizationVK/src/main/kotlin/AuthRoutes.kt
+++ authorizationVK/src/main/kotlin/AuthRoutes.kt
@@ -1,40 +1,26 @@
-import configFiles.Config
 import io.ktor.http.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import io.ktor.server.sessions.*
-import org.example.AuthService
-import org.example.CodeVerifierSession
 
 // Кэш для хранения параметров
 val parameterCache = mutableMapOf<String, Map<String, String>>()
 
 fun Route.authRoutes() {
-    route("/auth") {
-        get("/initiate") {
-            val (codeVerifier, codeChallenge, state) = AuthService.generatePKCE()
-            call.sessions.set(CodeVerifierSession(codeVerifier))
-            println("Code Verifier: $codeVerifier")
-            val authUrl = "https://id.vk.com/authorize?client_id=${Config.appConfig.CLIENT_ID}&redirect_uri=${Config.appConfig.REDIRECT_URI}&response_type=code&scope=email&code_challenge=$codeChallenge&code_challenge_method=S256&state=$state"
-            println("Auth URL: $authUrl")
-            call.respondText(authUrl) // Возвращаем ссылку на авторизацию
-        }
+    post("/auth/callback") {
+        // Извлекаем параметры из запроса
+        val code = call.parameters["code"] ?: return@post call.respond(HttpStatusCode.BadRequest, "Missing code parameter")
+        val deviceId = call.parameters["device_id"] ?: return@post call.respond(HttpStatusCode.BadRequest, "Missing device_id parameter")
+        val state = call.parameters["state"] ?: return@post call.respond(HttpStatusCode.BadRequest, "Missing state parameter")
 
-        post("/callback") {
-            // Извлекаем параметры из запроса
-            val code = call.parameters["code"] ?: return@post call.respond(HttpStatusCode.BadRequest, "Missing code parameter")
-            val deviceId = call.parameters["device_id"] ?: return@post call.respond(HttpStatusCode.BadRequest, "Missing device_id parameter")
-            val state = call.parameters["state"] ?: return@post call.respond(HttpStatusCode.BadRequest, "Missing state parameter")
+        println("Received state: $state") // Логируем полученный state
+        println("Received parameters: code=$code, device_id=$deviceId, state=$state")
 
-            println("Received parameters: code=$code, device_id=$deviceId, state=$state")
+        // Сохраняем параметры в кэш
+        val parameters = mapOf("code" to code, "device_id" to deviceId, "state" to state)
+        parameterCache[state] = parameters
+        println("Saved parameters: $parameters")
 
-            // Сохраняем параметры в кэш
-            val parameters = mapOf("code" to code, "device_id" to deviceId, "state" to state)
-            parameterCache[state] = parameters
-            println("Saved parameters: $parameters")
-
-            // Возвращаем 200 OK
-            call.respond(HttpStatusCode.OK)
-        }
+        // Возвращаем 200 OK
+        call.respond(HttpStatusCode.OK)
     }
 }
--- authorizationVK/src/main/kotlin/Main.kt
+++ authorizationVK/src/main/kotlin/Main.kt
@@ -7,14 +7,17 @@ import io.ktor.server.netty.*
 import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import io.ktor.server.sessions.*
 import io.ktor.server.websocket.*
 import io.ktor.websocket.*
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 import org.example.AuthService
-import org.example.CodeVerifierSession
 import java.io.File
+
+// Глобальные переменные для хранения данных
+var globalCodeVerifier: String? = null
+var globalState: String? = null
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
@@ -22,9 +25,6 @@ fun main() {
 }
 
 fun Application.module() {
-    install(Sessions) {
-        cookie<CodeVerifierSession>("SESSION")
-    }
     install(ContentNegotiation) {
         json()
     }
@@ -41,43 +41,53 @@ fun Application.frontModule(htmlBaseDir: File) {
         get("/auth") {
             call.respondFile(File(htmlBaseDir, "index.html"))
         }
-        authRoutes()
 
         webSocket("/ws") {
-            val (codeVerifier, codeChallenge, state) = AuthService.generatePKCE()
-            call.sessions.set(CodeVerifierSession(codeVerifier))
+            try {
+                // Генерируем PKCE
+                val (codeVerifier, codeChallenge, state) = AuthService.generatePKCE()
+                globalCodeVerifier = codeVerifier
+                globalState = state
+                println("Generated codeVerifier: $codeVerifier")
+                println("Generated state: $state") // Логируем сгенерированный state
 
-            val authUrl = "https://id.vk.com/authorize?client_id=${Config.appConfig.CLIENT_ID}&redirect_uri=${Config.appConfig.REDIRECT_URI}&response_type=code&scope=email&code_challenge=$codeChallenge&code_challenge_method=S256&state=$state"
+                // Генерируем URL авторизации
+                val authUrl = "https://id.vk.com/authorize?client_id=${Config.appConfig.CLIENT_ID}&redirect_uri=${Config.appConfig.REDIRECT_URI}&response_type=code&scope=email&code_challenge=$codeChallenge&code_challenge_method=S256&state=$state"
+                println("Auth URL: $authUrl")
+                send(Frame.Text(authUrl)) // Отправляем ссылку на авторизацию клиенту
 
-            println("Auth URL: $authUrl")
+                // Цикл ожидания параметров
+                while (isActive) {
+                    delay(4000) // Задержка перед проверкой кэша
+                    println("Checking cache for state: $state")
+                    val parameters = parameterCache[state]
+                    if (parameters != null) {
+                        val code = parameters["code"]
+                        val deviceId = parameters["device_id"]
 
-            while (true) {
-
-                println("Checking parameters for state: $state")
-
-                // Отправка сообщения "Await..." клиенту
-                send(Frame.Text("Await..."))
-
-                val parameters = parameterCache[state]
-                if (parameters != null) {
-                    val code = parameters["code"]
-                    val deviceId = parameters["device_id"]
-
-                    if (code != null && deviceId != null) {
-                        handleAuthorization(code, deviceId, state, codeVerifier) { response ->
-                            launch {
-                                send(Frame.Text(response))
+                        if (code != null && deviceId != null) {
+                            // Обработка авторизации
+                            handleAuthorization(code, deviceId, state, codeVerifier) { response ->
+                                if (this.isActive) {
+                                    launch {
+                                        println("Sending response: $response")
+                                        send(Frame.Text(response))
+                                    }
+                                }
                             }
+                            parameterCache.remove(state) // Удаляем параметры из кэша после обработки
+                            break
+                        } else {
+                            println("Code or Device ID is null for state: $state")
                         }
-
-                        break
                     } else {
-                        println("Code or Device ID is null for state: $state")
-                        send(Frame.Text("Code or Device ID is null for state: $state"))
+                        println("No parameters found for state: $state")
                     }
-                } else {
-                    println("No parameters found for state: $state") // Отладочное сообщение
-                    send(Frame.Text("No parameters found for state: $state"))
+                }
+            } catch (e: Exception) {
+                println("Error in WebSocket: ${e.message}")
+                if (this.isActive) {
+                    send(Frame.Text("Error: ${e.message}"))
                 }
             }
         }
